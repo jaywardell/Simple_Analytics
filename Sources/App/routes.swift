b85cfbaef759b61438db7ae461fd5d036940541c
Joseph Wardell <jaywardell@mac.com>
2023-02-28 13:05:44 -0500

    initial commit, with all routes removed

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,15 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+//    app.get { req async in
+//        "It works!"
+//    }
+//
+//    app.get("hello") { req async -> String in
+//        "Hello, world!"
+//    }
+//
+//    app.get("howdy") { req async -> String in
+//        "Howdy y'all!"
+//    }
 }
