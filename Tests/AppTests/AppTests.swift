ca2b61d74ff4131991ff8f469d1d5fa61348ccd7
Joseph Wardell <jaywardell@mac.com>
2023-02-28 13:58:54 -0500

    update AppTests to make sure that any old routes fail with a 404 error

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -2,14 +2,30 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
-    func testHelloWorld() throws {
+    func test_hello_returns_404() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
 
+        // the server should respond with a 404
+        // for any endpoint that isn't in
+        // the controllers we add
+        // so make sure that it does so for the original endpoint
+        // that was there when the project was created
         try app.test(.GET, "hello", afterResponse: { res in
-            XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
+            XCTAssertEqual(res.status, .notFound)
         })
     }
+    
+    func test_root_returns_404() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        // a request to root should return a 404
+        try app.test(.GET, "", afterResponse: { res in
+            XCTAssertEqual(res.status, .notFound)
+        })
+    }
+
 }
