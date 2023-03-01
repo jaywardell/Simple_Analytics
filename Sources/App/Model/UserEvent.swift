019ac5028e8e42783db15229644d0afce77f00da
Joseph Wardell <jaywardell@mac.com>
2023-03-01 09:29:32 -0500

    added a date property to UserEvent

--- Sources/App/Model/UserEvent.swift
+++ Sources/App/Model/UserEvent.swift
@@ -8,9 +8,26 @@
 import Vapor
 
 struct UserEvent: Content, Equatable {
-//    let date: Date
-//    
-//    init() {
-//        self.date = Date()
-//    }
+    let date: Date
+    
+    init() {
+        self.date = Date(timeIntervalSinceReferenceDate: Date().timeIntervalSinceReferenceDate.rounded())
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        if let date = try? container.decode(Date.self, forKey: .date) {
+            self.date = date
+        }
+        else if let dateDouble = try? container.decode(Double.self, forKey: .date) {
+            self.date = Date(timeIntervalSinceReferenceDate: dateDouble)
+        }
+        else if let dateString = try? container.decode(String.self, forKey: .date) {
+            let formatter = ISO8601DateFormatter()
+            self.date = formatter.date(from: dateString)!
+        }
+        else {
+            fatalError()
+        }
+    }
 }
