fe71c8d09f823dd7229fbca8d84dc5efb5e16e53
Joseph Wardell <jaywardell@mac.com>
2023-02-28 19:36:23 -0500

    added Fluent to Package.swift and configure.swift sets up the sqlite database in-memory

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,16 @@
 import Vapor
+import Fluent
+import FluentSQLiteDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    app.databases.use(.sqlite(.memory), as: .sqlite)
+    try app.autoMigrate().wait()
+
+    
     // register routes
     try routes(app)
 }
