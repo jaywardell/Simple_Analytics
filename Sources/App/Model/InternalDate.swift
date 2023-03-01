fc38ba61181cb4b17f9f05bc91355301941a3238
Joseph Wardell <jaywardell@mac.com>
2023-03-01 11:14:05 -0500

    added an InternalDate struct which always encodes and decodes its date as timeIntervalSinceReferenceDate rounded to the nearest second

---
+++ Sources/App/Model/InternalDate.swift
@@ -0,0 +1,28 @@
+//
+//  InternalDate.swift
+//  
+//
+//  Created by Joseph Wardell on 3/1/23.
+//
+
+import Foundation
+
+struct InternalDate: Codable, Equatable {
+    
+    let value: Date
+    
+    init(_ date: Date) {
+        self.value = Date(timeIntervalSinceReferenceDate: date.timeIntervalSinceReferenceDate.rounded())
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.singleValueContainer()
+        let dateDouble = try container.decode(Double.self)
+        self.value = Date(timeIntervalSinceReferenceDate: dateDouble.rounded())
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        try value.timeIntervalSinceReferenceDate.encode(to: encoder)
+    }
+}
