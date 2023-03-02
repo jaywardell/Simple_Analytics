4fbeb514daccb4bb58a315e19fe2a83a180c5a3f
Joseph Wardell <jaywardell@mac.com>
2023-03-02 08:43:22 -0500

    HeaderCheckingMiddlewareTestsController is only available while testing

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,5 +5,7 @@ func routes(_ app: Application) throws {
     // only use routes from the UserEventController
     try app.register(collection: UserEventController())
     
-    try app.register(collection: HeaderCheckingMiddlewareTestsController())
+    if app.environment == .testing {
+        try app.register(collection: HeaderCheckingMiddlewareTestsController())
+    }
 }
