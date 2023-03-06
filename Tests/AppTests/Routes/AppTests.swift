4cb82c7113d2e1d59218b3e3cc68b8cecaf5673c
Joseph Wardell <jaywardell@mac.com>
2023-03-06 09:15:07 -0500

    AppTests inherits from XCTVaporTests in order to get all the free setup and teardown that it provides. added a makeSUT() method that takes an optional configure method and returns a configured Application

--- Tests/AppTests/Routes/AppTests.swift
+++ Tests/AppTests/Routes/AppTests.swift
@@ -1,29 +1,27 @@
 @testable import App
 import XCTVapor
 
-final class AppTests: XCTestCase {
+final class AppTests: XCTVaporTests {
+    
+    override class func setUp() {
+        XCTVapor.app = { Application(.testing) }
+    }
+    
+    func makeSUT(configuration: ((Application) throws ->())? = nil) throws -> Application {
+        try (configuration ?? configure)(app)
+        return app
+    }
+    
     func test_hello_returns_404() throws {
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try configure(app)
 
-        // the server should respond with a 404
-        // for any endpoint that isn't in
-        // the controllers we add
-        // so make sure that it does so for the original endpoint
-        // that was there when the project was created
-        try app.test(.GET, "hello", afterResponse: { res in
+        try makeSUT().test(.GET, "hello", afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
     
     func test_root_returns_404() throws {
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try configure(app)
 
-        // a request to root should return a 404
-        try app.test(.GET, "", afterResponse: { res in
+        try makeSUT().test(.GET, "", afterResponse: { res in
             XCTAssertEqual(res.status, .notFound)
         })
     }
